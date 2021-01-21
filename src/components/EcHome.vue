b98336d55d5a171986f181b890dc057617e599d5
Leonardo Matos <leomp120894@gmail.com>
2021-01-21 19:15:27 -0300

    feat(ec-home): first (fixed) card with store main info and share (todo)

--- src/components/EcHome.vue
+++ src/components/EcHome.vue
@@ -1,5 +1,51 @@
 <template>
   <div class="ec-home">
+    <div class="card p-30 pt-40 text-center">
+      <div class="text-center mb-3">
+        <img
+          v-if="store.logo"
+          :src="store.logo.url"
+          alt="Logo"
+          class="img-fluid"
+          style="max-height: 60px"
+        >
+      </div>
+      <h1>{{ store.name }}</h1>
+
+      <p>
+        <a
+          :href="`mailto:${store.financial_email}`"
+          target="_blank"
+        >
+          {{ store.financial_email }}
+        </a>
+        <template v-if="store.domain">
+          ·
+          <a
+            :href="`https://${store.domain}`"
+            target="_blank"
+          >
+            {{ store.domain }}
+          </a>
+        </template>
+      </p>
+
+      <p>
+        <span class="badge badge-xl badge-light px-4">
+          <code class="fs-17">
+            <span class="text-light opacity-60">Store ID</span>
+            <span class="ml-3 text-secondary">{{ store.store_id }}</span>
+          </code>
+        </span>
+      </p>
+
+      <div class="gap-items fs-16">
+        <a class="text-facebook" href="#"><i class="fa fa-facebook"></i></a>
+        <a class="text-dribbble" href="#"><i class="fa fa-dribbble"></i></a>
+        <a class="text-google" href="#"><i class="fa fa-google"></i></a>
+        <a class="text-twitter" href="#"><i class="fa fa-twitter"></i></a>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/js/EcHome.js
+++ src/components/js/EcHome.js
@@ -3,16 +3,47 @@ import ecomAuth from '@ecomplus/auth'
 export default {
   name: 'EcHome',
 
+  props: {
+    ecomAuth: {
+      type: Object,
+      default () {
+        return ecomAuth
+      }
+    }
+  },
+
+  data () {
+    return {
+      store: {
+        store_id: null,
+        name: null,
+        homepage: null,
+        domain: null,
+        logo: null,
+        financial_email: null
+      }
+    }
+  },
+
   created () {
+    const { ecomAuth } = this
     const fetchStore = () => {
-      ecomAuth.fetchStore().then(store => {
-        console.log(store._id)
-      })
+      ecomAuth.fetchStore()
+        .then(store => {
+          for (const field in this.store) {
+            const val = store[field] || store.$main[field]
+            if (this.store[field] !== undefined && val) {
+              this.store[field] = val
+            }
+          }
+        })
+        .catch(console.error)
     }
     if (ecomAuth.checkLogin()) {
       fetchStore()
     } else {
       ecomAuth.on('login', fetchStore)
     }
+    ecomAuth.on('updateStore', fetchStore)
   }
 }
