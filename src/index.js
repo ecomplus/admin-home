70b4be07eb67c7dda65029de3179168dab9e9776
Leonardo Matos <leomp120894@gmail.com>
2021-01-24 16:24:40 -0300

    perf(app): stop importing unecessary bv plugins

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { ToastPlugin, VBTogglePlugin, ModalPlugin } from 'bootstrap-vue'
+import { ToastPlugin } from 'bootstrap-vue'
 
 export default () => {
   Vue.use(ToastPlugin)
-  Vue.use(VBTogglePlugin)
-  Vue.use(ModalPlugin)
 
   Vue.config.productionTip = false
 
