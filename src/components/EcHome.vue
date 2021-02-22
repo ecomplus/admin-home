4969db29a01ddf2210fbe92ba6934cbea10822e5
Leonardo Matos <leomp120894@gmail.com>
2021-02-22 15:45:27 -0300

    chore(ec-home): minor layout improvves

--- src/components/EcHome.vue
+++ src/components/EcHome.vue
@@ -7,164 +7,166 @@
       >
         <div class="row">
           <div class="col">
-            <div class="row align-items-center">
-              <div class="col">
-                <p>
-                  <span class="badge badge-xl badge-light px-4">
-                    <code
-                      class="fs-17"
-                      style="user-select: none"
-                    >
-                      <span class="text-light opacity-60">Store ID</span>
-                      <span
-                        class="pl-3 text-purple"
-                        style="user-select: text"
-                        v-text="storeId"
-                      />
-                    </code>
-                  </span>
-                </p>
+            <div class="d-flex flex-column justify-content-between h-100">
+              <div class="row align-items-center">
+                <div class="col">
+                  <p>
+                    <span class="badge badge-xl badge-light px-4">
+                      <code
+                        class="fs-17"
+                        style="user-select: none"
+                      >
+                        <span class="text-light opacity-60">Store ID</span>
+                        <span
+                          class="pl-3 text-purple"
+                          style="user-select: text"
+                          v-text="storeId"
+                        />
+                      </code>
+                    </span>
+                  </p>
 
-                <slide-y-up-transition>
-                  <div class="d-flex align-items-center mt-4 mb-3 mb-xl-0">
-                    <img
-                      v-if="store.logo"
-                      :src="store.logo.url"
-                      alt="Logo"
-                      class="img-fluid"
-                      style="max-height: 60px"
-                    >
-                    <a
-                      class="text-secondary ml-3"
-                      href="#"
-                      @click.prevent="uploadLogo"
-                      :title="i19uploadLogo"
-                    >
-                      <i class="ti-pencil"></i>
-                    </a>
-                  </div>
-                </slide-y-up-transition>
-              </div>
-
-              <div class="col-lg-7">
-                <slide-y-up-transition>
-                  <div v-if="store.name">
-                    <h1>{{ store.name }}</h1>
-
-                    <div>
+                  <slide-y-up-transition>
+                    <div class="d-flex align-items-center mt-4 mb-3 mb-xl-0">
+                      <img
+                        v-if="store.logo"
+                        :src="store.logo.url"
+                        alt="Logo"
+                        class="img-fluid"
+                        style="max-height: 60px"
+                      >
                       <a
-                        :href="`mailto:${store.financial_email}`"
-                        target="_blank"
+                        class="text-secondary ml-3"
+                        href="#"
+                        @click.prevent="uploadLogo"
+                        :title="i19uploadLogo"
                       >
-                        {{ store.financial_email }}
+                        <i class="ti-pencil"></i>
                       </a>
-
-                      <span
-                        v-if="store.domain"
-                        class="d-inline-block"
-                      >
-                        ·
-                        <a
-                          :href="`https://${store.domain}`"
-                          target="_blank"
-                          class="fw-500"
-                        >
-                          {{ store.domain }}
-                        </a>
-                        <a
-                          class="text-secondary ml-1"
-                          href="#"
-                          @click.prevent="isEditingDomain = !isEditingDomain"
-                          :title="i19setStoreDomain"
-                        >
-                          <i class="ti-pencil"></i>
-                        </a>
-                      </span>
                     </div>
+                  </slide-y-up-transition>
+                </div>
 
-                    <slide-y-up-transition>
-                      <div v-if="store.domain">
-                        <a
-                          target="_blank"
-                          :href="`https://${store.domain}/admin/`"
-                          class="btn btn-primary mr-3 mt-2"
-                        >
-                          <i class="ti-paint-roller"></i>
-                          <span class="ml-1 d-none d-md-inline">
-                            {{ i19editStorefront }}
-                          </span>
-                        </a>
-                        <a
-                          target="_blank"
-                          :href="shopLink"
-                          class="btn btn-primary mt-2"
-                        >
-                          <i class="ti-shopping-cart"></i>
-                          <span class="ml-1 d-none d-md-inline">
-                            {{ i19goToStore }}
-                          </span>
-                        </a>
-                      </div>
-                    </slide-y-up-transition>
+                <div class="col-lg-7">
+                  <slide-y-up-transition>
+                    <div v-if="store.name">
+                      <h1>{{ store.name }}</h1>
 
-                    <slide-y-up-transition>
-                      <div
-                        v-if="!store.domain || isEditingDomain"
-                        class="mt-3 form-type-combine"
-                      >
-                        <div class="form-group mb-0">
-                          <label>{{ i19domain }}</label>
-                          <input
-                            ref="input-domain"
-                            type="text"
-                            class="form-control"
-                            :placeholder="i19setStoreDomain"
-                            pattern="^([\w-]+\.){1,4}[\w]{2,}$"
-                            v-model="localDomain"
-                            @keyup.enter="setDomain"
-                            @focus="isEditingDomain = true"
-                            @blur="isEditingDomain = false"
+                      <div>
+                        <a
+                          :href="`mailto:${store.financial_email}`"
+                          target="_blank"
+                        >
+                          {{ store.financial_email }}
+                        </a>
+
+                        <span
+                          v-if="store.domain"
+                          class="d-inline-block"
+                        >
+                          ·
+                          <a
+                            :href="`https://${store.domain}`"
+                            target="_blank"
+                            class="fw-500"
                           >
-
-                          <small class="form-text">
-                            {{ domainInputHelpText }}
-                            <a
-                              target="_black"
-                              :href="`https://community.e-com.plus/search?q=${encodeURIComponent(i19domain)}`"
-                            >
-                              <i class="fa fa-question-circle"></i>
-                            </a>
-                          </small>
-                        </div>
+                            {{ store.domain }}
+                          </a>
+                          <a
+                            class="text-secondary ml-1"
+                            href="#"
+                            @click.prevent="isEditingDomain = !isEditingDomain"
+                            :title="i19setStoreDomain"
+                          >
+                            <i class="ti-pencil"></i>
+                          </a>
+                        </span>
                       </div>
-                    </slide-y-up-transition>
-                  </div>
-                </slide-y-up-transition>
-              </div>
-            </div>
 
-            <div class="d-none d-lg-block mt-4">
-              <a
-                href="/#/settings"
-                class="btn btn-sm btn-outline-secondary mr-3 mt-2"
-              >
-                <i class="fa fa-cogs mr-1"></i>
-                {{ i19settings }}
-              </a>
-              <a
-                href="/#/resources/products/new"
-                class="d-none d-xl-inline-block btn btn-sm btn-outline-secondary mr-3 mt-2"
-              >
-                <i class="fa fa-tag mr-1"></i>
-                {{ i19registerProduct }}
-              </a>
-              <a
-                href="/#/apps"
-                class="btn btn-sm btn-outline-secondary mt-2"
-              >
-                <i class="fa fa-puzzle-piece mr-1"></i>
-                {{ i19apps }}
-              </a>
+                      <slide-y-up-transition>
+                        <div v-if="store.domain">
+                          <a
+                            target="_blank"
+                            :href="`https://${store.domain}/admin/`"
+                            class="btn btn-primary mr-3 mt-2"
+                          >
+                            <i class="ti-paint-roller"></i>
+                            <span class="ml-1 d-none d-md-inline">
+                              {{ i19editStorefront }}
+                            </span>
+                          </a>
+                          <a
+                            target="_blank"
+                            :href="shopLink"
+                            class="btn btn-primary mt-2"
+                          >
+                            <i class="ti-shopping-cart"></i>
+                            <span class="ml-1 d-none d-md-inline">
+                              {{ i19goToStore }}
+                            </span>
+                          </a>
+                        </div>
+                      </slide-y-up-transition>
+
+                      <slide-y-up-transition>
+                        <div
+                          v-if="!store.domain || isEditingDomain"
+                          class="mt-3 form-type-combine"
+                        >
+                          <div class="form-group mb-0">
+                            <label>{{ i19domain }}</label>
+                            <input
+                              ref="input-domain"
+                              type="text"
+                              class="form-control"
+                              :placeholder="i19setStoreDomain"
+                              pattern="^([\w-]+\.){1,4}[\w]{2,}$"
+                              v-model="localDomain"
+                              @keyup.enter="setDomain"
+                              @focus="isEditingDomain = true"
+                              @blur="isEditingDomain = false"
+                            >
+
+                            <small class="form-text">
+                              {{ domainInputHelpText }}
+                              <a
+                                target="_black"
+                                :href="`https://community.e-com.plus/search?q=${encodeURIComponent(i19domain)}`"
+                              >
+                                <i class="fa fa-question-circle"></i>
+                              </a>
+                            </small>
+                          </div>
+                        </div>
+                      </slide-y-up-transition>
+                    </div>
+                  </slide-y-up-transition>
+                </div>
+              </div>
+
+              <div class="d-none d-lg-block mt-4">
+                <a
+                  href="/#/settings"
+                  class="btn btn-sm btn-outline-secondary mr-3 mt-2"
+                >
+                  <i class="fa fa-cogs mr-1"></i>
+                  {{ i19settings }}
+                </a>
+                <a
+                  href="/#/resources/products/new"
+                  class="d-none d-xl-inline-block btn btn-sm btn-outline-secondary mr-3 mt-2"
+                >
+                  <i class="fa fa-tag mr-1"></i>
+                  {{ i19registerProduct }}
+                </a>
+                <a
+                  href="/#/apps"
+                  class="btn btn-sm btn-outline-secondary mt-2"
+                >
+                  <i class="fa fa-puzzle-piece mr-1"></i>
+                  {{ i19apps }}
+                </a>
+              </div>
             </div>
           </div>
 
@@ -306,10 +308,10 @@
       >
     </div>
 
-    <template v-if="!isMobile">
+    <template v-if="!isMobile && ordersMetrics.countCreated">
       <slide-y-up-transition>
         <ec-orders-graphs
-          v-if="hasLoadedAllMetrics && ordersMetrics.countCreated"
+          v-if="hasLoadedAllMetrics"
           :date-range="fixedDateRange"
           @load="hasLoadedOrdersGraphs = hasLoadedOnce = true"
         />
